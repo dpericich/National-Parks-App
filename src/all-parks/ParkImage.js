3b938400dc8f5ec6c00b0b64dd5d8a48b014908c
Daniel <dpericich@gmail.com>
2020-10-24 10:22:57 -0500

    added helper function to determine if park pictures exist

--- src/all-parks/ParkImage.js
+++ src/all-parks/ParkImage.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import placeholder from './park-images/npsplaceholder.jpg';
 
-const ParkImage = ({park}) => {
+const ParkImage = ({park, selectPark, index}) => {
+
+    const setImage = (park) => {
+        if(park.images[0] === undefined) {
+            return placeholder
+        } else {
+            return park.images[0].url
+        }
+    }
+
     return(
-        <div className="park-title-image" style={{background : `center url(${park.images[0].url || placeholder} )`}}>
+        <div className="park-title-image" style={{background : `center url(${setImage(park)} )`}}  onClick={() => selectPark(index)}>
             <div>
                 {park.fullName}
             </div>
         </div>
     )
 };
+
 
 export default ParkImage;
