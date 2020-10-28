7de268ac349ef663c5832c40ef3aab8abb445bcc
Daniel <dpericich@gmail.com>
2020-10-27 22:51:02 -0500

    updated weather image dependency

--- src/weather-forecast/WeatherWidgetDay.js
+++ src/weather-forecast/WeatherWidgetDay.js
@@ -3,10 +3,12 @@ import PropTypes from 'prop-types';
 import {getWeatherTime, getWeatherImage} from './WeatherControllers'
 
 const WeatherWidgetDay = ({weatherStuff}) => {
+    const image = require(`${getWeatherImage(weatherStuff)}`)
+
     return(
         <div className="three-hour-container">
             <h1 className="three-hour-time">{getWeatherTime(weatherStuff)}</h1>
-            <img alt="weather" className="image-placeholder" src={require(`${getWeatherImage(weatherStuff)}`)}/>
+            <img alt="weather" className="image-placeholder" src={image}/>
             <p className="three-hour-high">{`HIGH : ${weatherStuff.main.temp_max}`}</p>
             <p className="three-hour-temp">{`CURRENT : ${weatherStuff.main.temp}`}</p>
             <p className="three-hour-low">{`LOW : ${weatherStuff.main.temp_min}`}</p>
